2a29f94411e1017f4ca06c299047e1cd38e24502
Milton Soares <eng.milton.soares@gmail.com>
2026-04-01 14:57:29 -0300

    ajuste: implementado o AgentResponse

--- app/agents/chat_agent/agent.py
+++ app/agents/chat_agent/agent.py
@@ -4,14 +4,20 @@ from app.core.models.llm import get_llm
 from app.agents.chat_agent.prompt import SYSTEM_PROMPT
 from app.agents.chat_agent.schema import ChatInput
 
+from app.core.models.response import AgentResponse
+
 class ChatAgent:
     def __init__(self):
         self.llm = get_llm()
     
-    def run(self, input_data: ChatInput) -> str:
+    def run(self, input_data: ChatInput) -> AgentResponse:
         messages = [
             SystemMessage(content=SYSTEM_PROMPT),
             HumanMessage(content=input_data.question)
         ]
         response = self.llm.invoke(messages)
-        return response
+        return AgentResponse(
+            answer=response,
+            source="chat",
+            success=True
+        )
