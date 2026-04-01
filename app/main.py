a542bdb2c8c3903d88f30a7cc60704c2420c08f0
Milton Soares <eng.milton.soares@gmail.com>
2026-04-01 16:07:12 -0300

    update da main para rodar o programa

--- app/main.py
+++ app/main.py
@@ -1,6 +1,28 @@
-from app.rag.ingest.ingest import run_ingestion
+from app.graph.graph import build_graph
+
 def main():
-    run_ingestion()
+    graph = build_graph()
+
+    state: dict = {
+        "question": "",
+        "history": [],
+        "last_username": None
+    }
+
+    while True:
+        question = input("\nAsk something (or 'exit'): ")
+
+        if question.lower() == "exit":
+            break
+        
+        state['question'] = question
+
+        result = graph.invoke(state)
+
+        print("\n--- RESPONSE ---:")
+        print(result["response"])
+
+        state = result
 
 if __name__ == "__main__":
     main()
